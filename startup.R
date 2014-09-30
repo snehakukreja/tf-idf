17c848601bf6cd507cb27d2c95eaeedb698f1e73
bmwilly <brandon.williams@kineticsocial.com>
2014-09-30 16:22:10 -0400

    initial commit

---
+++ startup.R
@@ -0,0 +1,47 @@
+
+# Startup file
+
+# load required packages
+require(RMySQL)
+require(ggplot2)
+require(ggthemes)
+require(zoo)
+require(data.table)
+require(ggvis)
+require(reshape2)
+require(plyr)
+require(dplyr)
+require(lubridate)
+require(rattle)
+require(caret)
+require(rpart)
+require(partykit)
+require(gbm)
+require(doParallel)
+require(pROC)
+require(corrplot)
+
+# clear global environment
+# rm(list=ls(all=TRUE))
+
+# don't want scientific notation
+options(scipen = 999)
+
+# close all MySQL connections
+for (con in dbListConnections(MySQL())) {dbDisconnect(con)}
+
+# connect to arcturus_demo database
+db <- dbConnect(
+  MySQL(),
+  user='bwilliams_ro',
+  password='pR3st!bRspej5zUb',
+  dbname='arcturus',
+  host='10.10.20.138',
+  port=3306
+)
+
+# create shorthand query function
+query <- function(...) dbGetQuery(db, ...)
+
+# attach arcturus_demo schema
+query("use arcturus;")
